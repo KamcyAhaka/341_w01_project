906458bfd8600c066e0e81473ec49af6ec662461
KamCy Ahaka <kamcyahaka@gmail.com>
2024-11-16 22:39:47 +0100

    docs: add Swagger API docs

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -2,6 +2,7 @@ const mongodb = require('../data/database');
 const ObjectId = require('mongodb').ObjectId;
 
 const getAll = async (req, res) => {
+  // #swagger.tags=['Contacts']
   const result = await mongodb.getDatabase().db().collection('contacts').find();
   result.toArray().then((contacts) => {
     res.setHeader('Content-Type', 'application/json');
@@ -10,6 +11,7 @@ const getAll = async (req, res) => {
 };
 
 const getSingle = async (req, res) => {
+  // #swagger.tags=['Contacts']
   const contactId = new ObjectId(req.params.id);
   const result = await mongodb.getDatabase().db().collection('contacts').find({ _id: contactId });
   result.toArray().then((contacts) => {
@@ -19,6 +21,7 @@ const getSingle = async (req, res) => {
 };
 
 const createContact = async (req, res) => {
+  // #swagger.tags=['Contacts']
   const contact = {
     firstName: req.body.firstName,
     lastName: req.body.lastName,
@@ -37,6 +40,8 @@ const createContact = async (req, res) => {
 };
 
 const updateContact = async (req, res) => {
+  // #swagger.tags=['Contacts']
+
   const contactId = new ObjectId(req.params.id);
 
   const contact = {
@@ -59,7 +64,9 @@ const updateContact = async (req, res) => {
     res.status(500).json(response.error || 'Some error occurred while updating the user');
   }
 };
+
 const deleteContact = async (req, res) => {
+  // #swagger.tags=['Contacts']
   const contactId = new ObjectId(req.params.id);
 
   const response = await mongodb
